6834e8690f94769f1754bccc44c7e64b5fd0be94
denisaldybaev-maker <denisaldybaev@gmail.com>
2026-09-17 16:34:37 +0500

    Update Main.kt
    додумана логика

--- Main.kt
+++ Main.kt
@@ -1,4 +1,4 @@
-//versions 1.2
+//versions 1.3
 fun main() {
     var hpPlayer = 100
     var hpDragon = 100
@@ -6,24 +6,24 @@ fun main() {
     val damagePlayer = 15
     var mercy = 3
     main@ while (true) {
-        val randomPotion = (1..100).random()
-        if (randomPotion == 50) {
-            if (hpPlayer == 100) {
-                println("вы нашли зелье, но вы здоровы")
-            } else if (hpPlayer <= 99) {
-                hpPlayer = 100
-                println("вы нашли зелье, ваше здоровье: $hpPlayer")
-            }
-        } else if (mercy >= 5) {
+        if (mercy >= 5) {
             println("вы добрый, дракон вас отпустил, нажмите enter чтобы выйти")
             readln()
             break@main
         }
+        val randomPotion = (1..100).random()
+        if (randomPotion == 50) {
+            if (hpPlayer == 100) {
+                println("вы нашли зелье, но вы здоровы")
+            } else {
+                hpPlayer = 100
+                println("вы нашли зелье, ваше здоровье: $hpPlayer")
+            }
+        }
         val dragonStrike = (1..mercy).random()
         when (dragonStrike) {
             3 -> {
-                hpPlayer -= damageDrakon
-                if (hpPlayer <= 0) hpPlayer = 0
+                hpPlayer = (hpPlayer - damageDrakon).coerceAtLeast(0)
                 println("вас ударил дракон. ваше хп: $hpPlayer")
                 if (hpPlayer < 1) {
                     println("вы проиграли, нажмите enter чтобы выйти")
@@ -36,21 +36,19 @@ fun main() {
         val choice = readln()
         when (choice) {
             "д" -> {
-                hpDragon -= damagePlayer
-                if (hpDragon <= 0) hpDragon = 0
+                hpDragon = (hpDragon - damagePlayer).coerceAtLeast(0)
                 println("вы ударили дракона. хп дракона: $hpDragon")
+                mercy = 3
                 if (hpDragon <= 0) {
                     println("вы победили, нажмите enter чтобы выйти")
                     readln()
                     break@main
                 }
             }
-
             "н" -> {
                 println("вы не ударили дракона")
                 mercy += 1
             }
-
             else -> println("ошибка")
         }
     }
