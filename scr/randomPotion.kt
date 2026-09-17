a80717ab146a2f324f806d2ea4cb9bdbebeef674
denisaldybaev-maker <denisaldybaev@gmail.com>
2026-09-17 23:11:26 +0500

    Update randomPotion.kt

--- scr/randomPotion.kt
+++ scr/randomPotion.kt
@@ -4,7 +4,7 @@ fun RandomPotion(hpPlayer: Int): Int {
         println("вы нашли зелье, но вы здоровы")
     } else {
         HpPlayer = (hpPlayer + 30).coerceAtMost(100)
-        println("вы нашли зелье, ваше здоровье: $hpPlayer")
+        println("вы нашли зелье, ваше здоровье: $HpPlayer")
     }
     return HpPlayer
 }
