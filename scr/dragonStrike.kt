1968f0d45dc862073da463115b6981822db9c377
denisaldybaev-maker <denisaldybaev@gmail.com>
2026-09-18 13:15:09 +0500

    Update dragonStrike.kt

--- scr/dragonStrike.kt
+++ scr/dragonStrike.kt
@@ -1,13 +1,9 @@
 fun dragonStrike(dragonStrike: Int, hpPlayer: Int,damageDragon: Int): Int {
     var HpPlayer = hpPlayer
-    if (hpPlayer == 0) {
-        println("вы проиграли, нажмите enter чтобы выйти")
-        readln()
-        return 101 //кодовое число controller.kt
-        if (dragonStrike == 1) {
-            HpPlayer = (hpPlayer - damageDragon).coerceAtLeast(0)
-            println("вас ударил дракон. ваше хп: $HpPlayer")
-        }
+    if (dragonStrike == 1) {
+        HpPlayer = (hpPlayer - damageDragon).coerceAtLeast(0)
+        println("вас ударил дракон. ваше хп: $HpPlayer")
     }
+
     return HpPlayer
 }
